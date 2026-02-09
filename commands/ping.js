c3f48b87e5f2300950525e24d27ceb36ced7ba6c
Zrefo <tomaszcichowicz07@gmail.com>
2026-02-09 23:15:54 +0100

    Delete commands directory

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'Sprawdza opóźnienie bota',
-    async execute(interaction) {
-        // Sprawdzenie opóźnienia bota
-        await interaction.reply(`Pong! 🏓 Latency: ${Date.now() - interaction.createdTimestamp}ms`);
-    }
-};
