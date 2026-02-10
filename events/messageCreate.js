b67fdb118dc98603513f3ce127f2fbb4c8f6aa9b
Zrefo <tomaszcichowicz07@gmail.com>
2026-02-10 09:54:46 +0100

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,33 +1,73 @@
 const fs = require('fs');
 const path = require('path');
+const { levelRoles, titleRoles } = require('../utils/levelRewards');
+
+const LEVEL_CHANNEL_ID = '1071227035147583498';
+const TITLE_CHANNEL_ID = '1400263230059708416';
 
 module.exports = {
     name: 'messageCreate',
     async execute(message) {
-        if (message.author.bot) return;
+        if (message.author.bot || !message.guild) return;
 
         const filePath = path.join(__dirname, '../data/levels.json');
         if (!fs.existsSync(filePath)) fs.writeFileSync(filePath, '{}');
 
-        const rawData = fs.readFileSync(filePath, 'utf8');
-        let data = {};
+        let data;
         try {
-            data = JSON.parse(rawData);
-        } catch (err) {
-            console.error('Błąd parsowania levels.json:', err);
+            data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        } catch {
             data = {};
         }
 
-        if (!data[message.author.id]) data[message.author.id] = { xp: 0, level: 1 };
+        if (!data[message.author.id]) {
+            data[message.author.id] = { xp: 0, level: 1 };
+        }
+
+        const userData = data[message.author.id];
+        const member = message.member;
 
         const gainedXP = Math.floor(Math.random() * 10) + 5;
-        data[message.author.id].xp += gainedXP;
+        userData.xp += gainedXP;
 
-        const nextLevelXP = Math.pow(data[message.author.id].level / 0.1, 2);
-        if (data[message.author.id].xp >= nextLevelXP) {
-            data[message.author.id].level += 1;
-            const channel = message.guild.channels.cache.get('1071227035147583498');
-            if (channel) channel.send(`> **<@${message.author.id}> Has reached the next level! \`${data[message.author.id].level}\`** 🎉`);
+        const nextLevelXP = Math.pow(userData.level / 0.1, 2);
+
+        if (userData.xp >= nextLevelXP) {
+            userData.level += 1;
+            const newLevel = userData.level;
+
+            const levelChannel = message.guild.channels.cache.get(LEVEL_CHANNEL_ID);
+            if (levelChannel) {
+                levelChannel.send(
+                    `> **${member} has reached the next level! \`${newLevel}\`** 🎉`
+                );
+            }
+
+            for (const roleId of Object.values(levelRoles)) {
+                if (member.roles.cache.has(roleId)) {
+                    await member.roles.remove(roleId).catch(() => {});
+                }
+            }
+
+            const newLevelRoleId = levelRoles[newLevel];
+            if (newLevelRoleId) {
+                await member.roles.add(newLevelRoleId).catch(() => {});
+            }
+
+            if (titleRoles[newLevel]) {
+                const title = titleRoles[newLevel];
+
+                if (!member.roles.cache.has(title.roleId)) {
+                    await member.roles.add(title.roleId).catch(() => {});
+
+                    const titleChannel = member.guild.channels.cache.get(TITLE_CHANNEL_ID);
+                    if (titleChannel) {
+                        titleChannel.send(
+                            `🏆 **${member} ${title.message}**`
+                        );
+                    }
+                }
+            }
         }
 
         fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
