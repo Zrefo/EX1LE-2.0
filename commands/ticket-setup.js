33602336ae571bd9b99b59f5500e136cc960cdf5
Zrefo <tomaszcichowicz07@gmail.com>
2026-02-10 03:49:56 +0100

    Update ticket-setup.js

--- commands/ticket-setup.js
+++ commands/ticket-setup.js
@@ -6,19 +6,16 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('ticket-setup')
         .setDescription('Setup ticket message')
-        .setDefaultMemberPermissions(0), // nikt nie może używać poza ID
+        .setDefaultMemberPermissions(0), 
 
     async execute(interaction) {
-        // Sprawdzenie ID użytkownika
         if (interaction.user.id !== '936390691603513374') {
             return interaction.reply({ content: 'You are not allowed to use this command!', ephemeral: true });
         }
 
-        // Wczytanie wiadomości z assets
         const ticketMessagePath = path.join(__dirname, '../assets/ticket-message.json');
         const ticketData = JSON.parse(fs.readFileSync(ticketMessagePath, 'utf8'));
 
-        // Wysłanie wiadomości z przyciskiem
         const channel = interaction.guild.channels.cache.get('1279081982579707914');
         if (!channel) return interaction.reply({ content: 'Channel not found!', ephemeral: true });
 
@@ -27,3 +24,4 @@ module.exports = {
         interaction.reply({ content: 'Ticket system has been set up!', ephemeral: true });
     }
 };
+
